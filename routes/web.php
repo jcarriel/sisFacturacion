db6bd64cc99fe730af170259d96b4986b18c4159
Jeampier Carriel <jcarrielroca@gmail.com>
2021-06-22 20:59:59 -0500

    pilas

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,16 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('auth/login');
 });
 
-Route::resource('almacen/categoria','CategoriaController');
-Route::resource('almacen/articulo','ArticuloController');
-Route::resource('proyectos','ProyectoController');
-Route::resource('ventas/cliente','ClienteController');
-Route::resource('ventas/venta','VentaController');
-Route::resource('banco','CuentaController');
-Route::resource('compras/proveedor','ProveedorController');
-Route::resource('compras/ingreso','IngresoController');
-Route::resource('seguridad/usuario','UsuarioController');
-Route::resource('cotizaciones','CotizacionController');
-Route::resource('pedidos','PedidoController');
-Route::resource('/pdf','PdfController'); 
-Route::get('crear_reporte_porventa/{tipo}','PdfController@crear_reporte_porventa'); 
-Route::get('/cotizacion/reporte/{id}', 'CotizacionController@crear_pdf');
-Route::get('/ventas/venta/reporte/{id}', 'VentaController@crear_pdf');
-Route::get('/pedidos/reporte/{id}', 'PedidoController@crear_pdf');
+Route::get('/articulos', 'ArticulosController@index')->name('articulos');
+Route::get('/articulos/new', 'ArticulosController@index')->name('articulos');
+
+
+
 Route::get('/logout', 'Auth\LoginController@logout');
 Route::get('/clear-cache', function() {
     Artisan::call('cache:clear');
